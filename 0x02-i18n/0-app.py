099227ac396501df6cc0de80583a59341cef19bc
St Pardon <okc4pardon@gmail.com>
2023-02-28 19:11:20 +0100

    :white_check_mark: completed task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Flask application
+'''
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    '''the base route
+    '''
+    return render_template('0-index.html')
